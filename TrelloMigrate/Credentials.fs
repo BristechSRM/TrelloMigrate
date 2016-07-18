079b323c553a81e525c4f9054849dd60e7904f71
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-18 10:11:10 +0100

    Initial Solution. Basic Cards fetch

---
+++ TrelloMigrate/Credentials.fs
@@ -0,0 +1,18 @@
+﻿module Credentials
+
+open System
+open System.Configuration
+
+type TrelloApiCredentials = 
+    { Key : string
+      Token : string }
+
+let getTrelloCredentials() = 
+    let creds = 
+        { Key = ConfigurationManager.AppSettings.Item("TrelloKey")
+          Token = ConfigurationManager.AppSettings.Item("TrelloToken") }
+
+    if String.IsNullOrWhiteSpace creds.Key || String.IsNullOrWhiteSpace creds.Token then 
+        failwith 
+            "TrelloKey or TrelloToken is missing. Use the TrelloCredsTemplate.config template to create a TrelloCreds.config, and add the values for your Trello Api Key and Token"
+    else creds
---
+++ TrelloMigrate/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.Net
+open System.Net.Http
+
+[<EntryPoint>]
+let main argv = 
+    try 
+        let trelloCred = Credentials.getTrelloCredentials()
+        let cardsUri = 
+            Uri <| sprintf "https://api.trello.com/1/boards/524ec750ed130abd230011ab/cards/open?fields=id,name,idMembers,due&key=%s&token=%s" trelloCred.Key trelloCred.Token
+        use client = new HttpClient()
+        let result = client.GetAsync(cardsUri).Result
+        match result.StatusCode with
+        | HttpStatusCode.OK -> 
+            let json = result.Content.ReadAsStringAsync().Result
+            printfn "%s" json
+        | errorCode -> 
+            let errorMessage = result.Content.ReadAsStringAsync().Result
+            failwith <| sprintf "Error fetching cards from srm board. Status code: %i. Reason phrase: %s. Error Message: %s" (int (errorCode)) result.ReasonPhrase errorMessage
+        0
+    with ex -> 
+        printfn "%A" ex
+        1
