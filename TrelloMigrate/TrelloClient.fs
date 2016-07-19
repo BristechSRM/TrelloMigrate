2c5fbe3032c2644a4a50c3a56facf429b1fd325d
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-19 16:43:22 +0100

    Split member grouping to separate function

--- TrelloMigrate/TrelloClient.fs
+++ TrelloMigrate/TrelloClient.fs
@@ -11,14 +11,16 @@ let private getBasicCards trelloCred =
     |> JsonHttpClient.get<BasicCard []>
 
 let private getBasicMembers trelloCred = 
-    let ignoredAdminUserNames = [ "samdavies"; "jamesphillpotts"; "tamarachehayebmakarem1"; "tamaramakarem"; "nicholashemley" ]
-
     sprintf "https://api.trello.com/1/boards/524ec750ed130abd230011ab/members?fields=id,username,fullName,avatarHash&key=%s&token=%s" trelloCred.Key trelloCred.Token
     |> Uri
     |> JsonHttpClient.get<BasicMember []>
+
+let private groupMembers (members) = 
+    let ignoredAdminUserNames = [ "samdavies"; "jamesphillpotts"; "tamarachehayebmakarem1"; "tamaramakarem"; "nicholashemley" ]
+    members
     |> Array.partition (fun bMember -> ignoredAdminUserNames |> List.contains bMember.Username)
     |> (fun (ignoredMembers, keptMembers) -> { Members = keptMembers; IgnoredMembers = ignoredMembers } )
 
 let getBoardSummary trelloCred =     
     { BasicCards = getBasicCards trelloCred
-      GroupedMembers = getBasicMembers trelloCred }
+      GroupedMembers = getBasicMembers trelloCred |> groupMembers}
