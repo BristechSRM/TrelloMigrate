b53a7baa3faceb7ce5dc7347173166c31e74be14
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-20 14:20:00 +0100

    Add post function to httpclient

--- TrelloMigrate/JsonHttpClient.fs
+++ TrelloMigrate/JsonHttpClient.fs
@@ -4,15 +4,29 @@ open System
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
+open System.Text
 
 let get<'Model> (uri : Uri) = 
     use client = new HttpClient()
-    let result = client.GetAsync(uri).Result
-    match result.StatusCode with
+    let response = client.GetAsync(uri).Result
+    match response.StatusCode with
     | HttpStatusCode.OK -> 
-        let json = result.Content.ReadAsStringAsync().Result
+        let json = response.Content.ReadAsStringAsync().Result
         JsonConvert.DeserializeObject<'Model>(json)
     | errorCode -> 
-        let errorMessage = result.Content.ReadAsStringAsync().Result
+        let errorMessage = response.Content.ReadAsStringAsync().Result
         let modelName = typeof<'Model>.Name
-        failwith <| sprintf "Error in get request for %s. Status code: %i. Reason phrase: %s. Error Message: %s" modelName (int (errorCode)) result.ReasonPhrase errorMessage
+        failwith <| sprintf "Error in get request for %s. Status code: %i. Reason phrase: %s. Error Message: %s" modelName (int (errorCode)) response.ReasonPhrase errorMessage
+
+let post (uri : Uri) (data : 'Model) = 
+    use client = new HttpClient()
+    let jsonData = JsonConvert.SerializeObject(data)
+    let content = new StringContent(jsonData, Encoding.UTF8, "application/json")
+    let response = client.PostAsync(uri, content).Result
+    match response.StatusCode with
+    | HttpStatusCode.Created -> response.Content.ReadAsStringAsync().Result
+    | errorCode -> 
+        let errorMessage = response.Content.ReadAsStringAsync().Result
+        let modelName = typeof<'Model>.Name
+        failwith <| sprintf "Error in post request for %s. Status code: %i. Reason phrase: %s. Error Message: %s" modelName (int (errorCode)) response.ReasonPhrase errorMessage
+
