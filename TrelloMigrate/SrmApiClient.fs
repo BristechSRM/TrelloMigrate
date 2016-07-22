029c964d508dfdabdfd05ae34d0399e07b42e9ea
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-22 14:18:41 +0100

    Refactor SrmAPiClient

--- TrelloMigrate/SrmApiClient.fs
+++ TrelloMigrate/SrmApiClient.fs
@@ -5,25 +5,24 @@ open System
 open SrmApiModels
 open JsonHttpClient
 
-let private parseQuotedGuid (guidString : string) = 
-    Guid.Parse(guidString.Replace("\"",""))
+let private parseQuotedGuid (guidString : string) = Guid.Parse(guidString.Replace("\"",""))
+
+let postAndGetGuid uri data = post uri data |> parseQuotedGuid
 
 module Profile = 
-    let endpoint = Uri(sessionsServiceUri, "Profiles")
+    let private endpoint = Uri(sessionsServiceUri, "Profiles")
 
     let postAndGetId (profile : Profile) = 
-        let result = post endpoint profile
-        { profile with Id = parseQuotedGuid result }
+        { profile with Id = postAndGetGuid endpoint profile }
 
 module Handle = 
-    let endpoint = Uri(sessionsServiceUri, "Handles")
+    let private endpoint = Uri(sessionsServiceUri, "Handles")
 
     let post (handle : Handle) = 
         post endpoint handle |> ignore
 
 module Session = 
-    let endpoint = Uri(sessionsServiceUri, "Sessions")
+    let private endpoint = Uri(sessionsServiceUri, "Sessions")
 
     let postAndGetId (session : Session) = 
-        let result = post endpoint session
-        { session with Id = parseQuotedGuid result } 
+        { session with Id = postAndGetGuid endpoint session} 
