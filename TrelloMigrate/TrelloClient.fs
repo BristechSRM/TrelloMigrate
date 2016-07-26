fe36d9dee693aea09a04780ed8897e2aa95d5090
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-26 10:30:20 +0100

    Fetch Card actions from trello

--- TrelloMigrate/TrelloClient.fs
+++ TrelloMigrate/TrelloClient.fs
@@ -15,6 +15,18 @@ let private getBasicMembers trelloCred =
     |> Uri
     |> JsonHttpClient.get<BasicMember []>
 
-let getBoardSummary trelloCred =     
-    { BasicCards = getBasicCards trelloCred
+let private getBasicActionsByCardId trelloCred (cardId : string) = 
+    sprintf "https://api.trello.com/1/cards/%s/actions?filter=commentCard&key=%s&token=%s" cardId trelloCred.Key trelloCred.Token
+    |> Uri
+    |> JsonHttpClient.get<BasicAction []>
+
+let getBoardSummary trelloCred = 
+    let basicCards = getBasicCards trelloCred    
+    let cardActions = 
+        basicCards
+        |> Array.map (fun card -> card.Id, getBasicActionsByCardId trelloCred card.Id)
+        |> Array.filter (fun (_, actions) -> actions.Length > 0)
+        |> Map.ofArray
+    { BasicCards = basicCards
+      CardActions = cardActions
       Members = getBasicMembers trelloCred }
